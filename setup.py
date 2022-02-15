d7ab3fa0725a79b37ce6ee549281d07b02b860b7
Chivier Humber <chivier.humber@outlook.com>
2022-02-15 12:05:06 +0800

    Update pypi package

--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@ import pathlib
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="enview",
-    version="1.0.0",
+    version="1.0.1",
     author="Chivier Humber",
     install_requires=[
         'click==8.0.3',
@@ -27,10 +30,14 @@ setup(
         'typing_extensions==4.0.1',
         'wcwidth==0.2.5'
     ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Chivier/enview",
+    author_email="chivier.humber@outlook.com",
     entry_points={
         "console_scripts": ["enview=enview.enview:main"],
     },
-    license="MIT",
+    license="Apache-2.0 License",
     keywords="translator",
     packages=find_packages(),
 )
