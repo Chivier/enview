6b6824dbc96ab75e5bf32376365b2334e4d6f686
Chivier Humber <chivier.humber@outlook.com>
2025-07-17 17:34:34 +0100

    Build: bump version 1.0.4 -> 1.0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as fh:
 
 setup(
     name="enview",
-    version="1.0.4",
+    version="1.0.5",
     author="Chivier Humber",
     install_requires=[
         "click>=8.0.3,<9.0.0",
